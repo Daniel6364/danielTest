b97cb083b2fde46a48fbdaf043c43a71290c9b9c
sunghwan Kim <iamsung33@neighbor21.co.kr>
2022-05-02 20:35:34 +0900

    Initial commit
    Preparing for RBAC

---
+++ src/test/java/com/example/danieltest/DanielTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danieltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
